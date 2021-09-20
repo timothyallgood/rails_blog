a92c76d6ffc85f0bbf94865db5970277d708543e
Timothy Allgood <tpaj0818@gmail.com>
2021-09-20 09:02:04 -0500

    added comment create action, added form to create and display comments on article show view

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params) # creates comment associated with specific article
+        redirect_to article_path(@article)
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index" # Sets our index action from our controller to display as the homepage
  
-  resources :articles # Rails route method to generate all convetional routes for our Articles controller
+  resources :articles do # Rails route method to generate all conventional routes for our Articles controller
+    resources :comments # Adds conventional routes for comments on each article
+  end
 end
