5273cbae1611af5c95e693d8eb044d3e4357639d
Timothy Allgood <tpaj0818@gmail.com>
2021-09-19 22:44:54 -0500

    added controller actions for article creation routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show # Controller action to show one article using an id param passed from our route, /articles/:id
     @article = Article.find(params[:id])
   end
+
+  def new # Instantaites empty article, will render our form view
+    @article = Article.new
+  end
+
+  def create #  Instantaites article and attempts to save it
+    @article = Article.new(title: "...", body: '...')
+
+    if @article.save # Saves article, if successful redirects to article show page
+      redirect_to @article
+    else # If unsuccesful, redisplay form
+      render :new 
+  end
+
 end
