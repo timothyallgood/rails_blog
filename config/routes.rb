f09b482633c6b5b473cb637f5400b1667e94db2f
Timothy Allgood <tpaj0818@gmail.com>
2021-09-19 22:09:24 -0500

    refactored routes and articles index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index" # Sets our index action from our controller to display as the homepage
  
-  get "/articles", to: "articles#index" # Associates the /articles route to the index action of our Articles Controller
-
-  get "/articles/:id", to: "articles#show" # When /articles/:id is visited we will use our show action to find the article with the associated id
+  resources :articles # Rails route method to generate all convetional routes for our Articles controller
 end
