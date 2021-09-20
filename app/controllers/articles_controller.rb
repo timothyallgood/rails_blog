c7e5129b8820e2d592aceb14ec2d5728176510ff
Timothy Allgood <tpaj0818@gmail.com>
2021-09-19 21:54:48 -0500

    setup /articles/:id route, added article show controller action, created show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-  def index
+  def index # Controller action to show all articles
     @articles = Article.all
+  end
+
+  def show # Controller action to show one article using an id param passed from our route, /articles/:id
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "articles#index" # Sets our index action from our controller to display as the homepage
  
   get "/articles", to: "articles#index" # Associates the /articles route to the index action of our Articles Controller
+
+  get "/articles/:id", to: "articles#show" # When /articles/:id is visited we will use our show action to find the article with the associated id
 end
