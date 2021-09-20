bf7f6614fb041c7434a3ded8520d31e23f20a200
Timothy Allgood <tpaj0818@gmail.com>
2021-09-19 21:43:02 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rails Blog
+
+## About
+
+This will be a learning project to learn Ruby and Ruby on Rails. 
+
+It will be a very simple blog using Rails as the framework. 
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root "articles#index" # Sets our index action from our controller to display as the homepage
+ 
+  get "/articles", to: "articles#index" # Associates the /articles route to the index action of our Articles Controller
+end
